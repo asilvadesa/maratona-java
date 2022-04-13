8eb031e87e99086ea6052b622f080c567caa8ee9
Anderson Silva de Sa <asilvadesa@gmail.com>
2022-04-12 22:41:27 -0300

    96 - Excecoes pt 02 - RuntimeException

--- src/academy/devdojo/maratonajava/javacore/OException/test/error/StackOverflowTest01.java
+++ src/academy/devdojo/maratonajava/javacore/OException/test/error/StackOverflowTest01.java
@@ -1,4 +1,4 @@
-package academy.devdojo.maratonajava.javacore.OException.test;
+package academy.devdojo.maratonajava.javacore.OException.test.error;
 
 public class StackOverflowTest01 {
     public static void main(String[] args) {
---
+++ src/academy/devdojo/maratonajava/javacore/OException/test/runtime/RuntimeExceptionTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.OException.test.runtime;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        //Checked and unchecked
+
+        Object obj = null;
+        System.out.println(obj.toString());
+    }
+}
